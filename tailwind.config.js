92e63f59cc7a6250b04d1e87edc932741083082d
Renzo <renzojacobtamayo@gmail.com>
2025-11-14 01:12:43 +0800

    Added Tailwind CSS setup to project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js}",
+    "./apps/**/*.{html,js,py}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
